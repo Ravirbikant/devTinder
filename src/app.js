8913f73f36d538c02468f364c9ff554e7a5d56af
Ravi Kant Jha <ravirbikant@gmail.com>
2025-12-06 11:14:13 +0530

    Adding post user and saving user to DB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
+const User = require("./models/user.js");
 const app = express();
+
+app.get("/health", (req, res) => {
+  res.send("App is running.");
+});
+
+app.post("/user", async (req, res) => {
+  const user = new User({
+    firstName: "Ravi",
+    lastName: "Kant Jha",
+    emailId: "ravirbikant@gmail.com",
+    password: "Abcd",
+    age: "28",
+    gender: "Male",
+  });
+
+  try {
+    await user.save();
+    res.send("New user added successfully");
+  } catch (err) {
+    res.status(400).send("User could not be added " + err);
+  }
+});
 
 connectDB()
   .then(() => {
