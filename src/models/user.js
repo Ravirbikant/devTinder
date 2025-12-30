a327d8c783718ba86ebcc202e3a2c2894b7497e3
Ravi Kant Jha <ravirbikant@gmail.com>
2025-12-30 13:18:07 +0530

    Adding different validations and timestamp

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,43 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate: function (value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Value can either be male, female or others only.");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+      default: ["cricket"],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
