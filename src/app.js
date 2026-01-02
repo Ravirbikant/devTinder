1366747e87563dbb2cade9e21449cd1af5889399
Ravi Kant Jha <ravirbikant@gmail.com>
2026-01-02 23:29:47 +0530

    Creating userAuth middleware and exploring toeken and cookie expiry

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const SPECIAL_KEY = "DEV@Tinder$790";
+const { userAuth } = require("./middlewares/admin.js");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -70,9 +71,13 @@ app.post("/login", async (req, res) => {
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) throw new Error("Invalid credentials");
 
-    const token = await jwt.sign({ _id: user._id }, SPECIAL_KEY);
+    const token = await jwt.sign({ _id: user._id }, SPECIAL_KEY, {
+      expiresIn: "1h",
+    });
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
 
     res.send("Login successful");
   } catch (error) {
@@ -80,15 +85,9 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const { token } = req.cookies;
-    if (!token) throw new Error("Invalid credentials");
-
-    const { _id } = await jwt.verify(token, SPECIAL_KEY);
-
-    const user = await User.findOne({ _id: _id });
-
+    const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("Error getting profile : " + err);
--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,8 +1,23 @@
-const adminAuth = (req, res, next) => {
-  const token = "abc";
-  const isUserAdmin = token === "abc";
-  if (!isUserAdmin) res.status(401).send("Unauthorized request");
-  next();
+const jwt = require("jsonwebtoken");
+const SPECIAL_KEY = "DEV@Tinder$790";
+const User = require("../models/user.js");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) throw new Error("No token found");
+
+    const { _id } = jwt.verify(token, SPECIAL_KEY);
+    const user = await User.findById(_id);
+
+    if (!user) throw new Error("No user found");
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("Error : " + err);
+  }
 };
 
-module.exports = { adminAuth };
+module.exports = { userAuth };
